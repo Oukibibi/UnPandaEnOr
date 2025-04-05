23d61e29875cac175b99f6559e817cb4a54c60f8
Hookibibi <115881751+Oukibibi@users.noreply.github.com>
2025-04-05 15:54:48 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import questionsData from './questions.json';
 function App() {
 
   const [questionNumber, setQuestionNumber] = useState(0);
+
   const [teamBluePoints, setTeamBluePoints] = useState(0);
   const [teamRedPoints, setTeamRedPoints] = useState(0);
+  const [teamGreenPoints, setTeamGreenPoints] = useState(0);
+
   const [currentAnswerToDisplay, setCurrentAnswerToDisplay] = useState([]);
   const [currentPointsAwarded, setCurrentPointsAwarded] = useState([]);
 
@@ -18,6 +21,12 @@ function App() {
     setQuestionNumber(questionNumber + 1)
   }
 
+  function previousQuestion() {
+    setCurrentPointsAwarded([])
+    setCurrentAnswerToDisplay([])
+    setQuestionNumber(questionNumber - 1)
+  }
+
   function toggleAnswer(response) {
     setCurrentAnswerToDisplay(currentAnswerToDisplay => ({
       ...currentAnswerToDisplay,
@@ -27,11 +36,6 @@ function App() {
 
   return (
     <div className="App">
-      <div className="blueTeam">
-        <p>Equipe Mirai</p>
-        <p>{teamBluePoints}</p>
-        {showDebug && <input type='number' onChange={e => setTeamBluePoints(parseInt(e.target.value))}/>}
-      </div>
 
       <div className="currentQuestion">
         <p id="questionNumber">Question N°{questionNumber + 1}</p>
@@ -40,19 +44,21 @@ function App() {
           if (currentAnswerToDisplay[index])
           {
             return (<div className='reponseBlock' key={index}>
-              {!currentPointsAwarded[index] && <button onClick={() => { 
-                  setTeamBluePoints(teamBluePoints + element.points) 
-                  setCurrentPointsAwarded(currentPointsAwarded => ({
-                    ...currentPointsAwarded,
-                    [index]: !currentPointsAwarded[index]
-                  }));
-                }} id="buttonBlue">&lt;</button>}
               <div className='reponse' onClick={() => toggleAnswer(index)}>
                 <p className='reponseText'>{element.reponse}</p>
                 <div className='reponsePointsBlock'> 
                   <p className='reponsePoints'>{element.points}</p>
                 </div>
               </div>
+              <div className='buttons'>
+
+              {!currentPointsAwarded[index] && <button onClick={() => { 
+                  setTeamBluePoints(teamBluePoints + element.points) 
+                  setCurrentPointsAwarded(currentPointsAwarded => ({
+                    ...currentPointsAwarded,
+                    [index]: !currentPointsAwarded[index]
+                  }));
+                }} id="buttonBlue">&gt;</button>}
               
               {!currentPointsAwarded[index] && <button onClick={() => { 
                   setTeamRedPoints(teamRedPoints + element.points) 
@@ -61,6 +67,15 @@ function App() {
                     [index]: !currentPointsAwarded[index]
                   }));
                 }} id="buttonRed">&gt;</button>}
+
+              {!currentPointsAwarded[index] && <button onClick={() => { 
+                  setTeamGreenPoints(teamGreenPoints + element.points) 
+                  setCurrentPointsAwarded(currentPointsAwarded => ({
+                    ...currentPointsAwarded,
+                    [index]: !currentPointsAwarded[index]
+                  }));
+                }} id="buttonGreen">&gt;</button>}
+              </div>
             </div>
             )
           }
@@ -68,16 +83,37 @@ function App() {
             <div className='reponseYetToDiscover' onClick={() => toggleAnswer(index)}>
               <p>?</p>
             </div>
+            <div className='buttons'></div>
           </div>
           )
         })}
+        <div className='questionsButtons'>
+        {(questionNumber - 1) >= 0 && <button onClick={previousQuestion}>Question précédente</button>}
         {(questionNumber + 1) < questionsData.questions.length && <button onClick={nextQuestion}>Question suivante</button>}
+        </div>
       </div>
 
-      <div className="redTeam">
-        <p>Equipe Hooki</p>
-        <p>{teamRedPoints}</p>
-        {showDebug && <input type='number' onChange={e => setTeamRedPoints(parseInt(e.target.value))}/>}
+      <div className='points'>
+
+
+        <div className="blueTeam">
+          <p>Equipe Mirai</p>
+          <p>{teamBluePoints}</p>
+          {showDebug && <input type='number' onChange={e => setTeamBluePoints(parseInt(e.target.value))}/>}
+        </div>
+
+        <div className="redTeam">
+          <p>Equipe Hooki</p>
+          <p>{teamRedPoints}</p>
+          {showDebug && <input type='number' onChange={e => setTeamRedPoints(parseInt(e.target.value))}/>}
+        </div>
+        
+
+        <div className="greenTeam">
+          <p>Equipe Drako</p>
+          <p>{teamGreenPoints}</p>
+          {showDebug && <input type='number' onChange={e => setTeamGreenPoints(parseInt(e.target.value))}/>}
+        </div>
       </div>
 
       <button onClick={() => setShowDebug(!showDebug)} id="debugButton">Debug</button>
